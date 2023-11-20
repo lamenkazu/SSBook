f87c5bf8cae811212ad184f0e91cb0c659beb2c5
Erick Etiene <333.erick@gmail.com>
2023-11-20 10:43:33 -0300

    Deploy Fix Finished

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["js-big-decimal"],
   },
-  base: "./",
+  base: "/SSBook",
 });
